584842c1039c37040d04d9525c0979fb3a46f079
oscarvm <ovalor@gmail.com>
2019-05-15 18:55:05 +0200

    PLANTILLAS Y CONTROLADORESF

---
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -0,0 +1,39 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//con esta primera anotación le indicamos que spring tiene que tratar este beam como un controlador
+@Controller
+
+//le indicamos que el controlador es de tipo mapeo
+@RequestMapping("/example")
+public class ExampleController {
+	
+		//nos definimos el nombre de la vista como una cte, para no tener que repetirla
+		//en un proyecto grande nos construirimamos todos los nombres de la vista en otra
+		//clase como ctes.
+		public static final String EXAMPLE_VIEW = "example";
+
+		//Primera forma. Nos creamos un método que nos retorne la vista
+		//esta anotación es es una hija de requestmapping donde se le indica que el request method es get
+		//ESTA FORMA SE UTILIZA CUANTO TENGO QUE INSERTAR POCOS DATOS EN LA VISTA
+		@GetMapping("/exampleString")	
+
+		public String exampleString() {
+			//indicamos la vista a devolver
+			return EXAMPLE_VIEW;
+		}
+		
+		@GetMapping("/exampleMAV")	
+		//Segunda forma. Mediante invocacion al método modelo-vista de spring
+		//que devuelve un modelo y una vista
+		//ESTA OTRA FORMA SE SUELE UTILIZAR PARA CUANDO HAY QUE INSERTAR MUCHOS DATOS
+		
+		public ModelAndView exampleMAV() {
+			return new ModelAndView(EXAMPLE_VIEW);
+		}
+}
