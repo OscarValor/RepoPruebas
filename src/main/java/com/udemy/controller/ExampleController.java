c76dfa110f95022b73aea04e4fcfbc3a18ef1734
oscarvm <ovalor@gmail.com>
2019-05-15 19:13:12 +0200

    AÑADO CAMPOS A LA VISTAS

--- src/main/java/com/udemy/controller/ExampleController.java
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -1,9 +1,9 @@
 package com.udemy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 //con esta primera anotación le indicamos que spring tiene que tratar este beam como un controlador
@@ -23,17 +23,23 @@ public class ExampleController {
 		//ESTA FORMA SE UTILIZA CUANTO TENGO QUE INSERTAR POCOS DATOS EN LA VISTA
 		@GetMapping("/exampleString")	
 
-		public String exampleString() {
+		public String exampleString(Model model) {
+			
+			model.addAttribute("name", "Jon");
 			//indicamos la vista a devolver
 			return EXAMPLE_VIEW;
 		}
 		
-		@GetMapping("/exampleMAV")	
 		//Segunda forma. Mediante invocacion al método modelo-vista de spring
 		//que devuelve un modelo y una vista
 		//ESTA OTRA FORMA SE SUELE UTILIZAR PARA CUANDO HAY QUE INSERTAR MUCHOS DATOS
-		
+		@GetMapping("/exampleMAV")
 		public ModelAndView exampleMAV() {
-			return new ModelAndView(EXAMPLE_VIEW);
+			
+			ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+			mav.addObject("name","Mikel");
+			
+			return mav;
+			//return new ModelAndView(EXAMPLE_VIEW);
 		}
 }
